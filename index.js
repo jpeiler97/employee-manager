20fd6d2c6e389e8b9f6e34d9ad1f8518f890fd62
JP Eiler <jp.eiler97@gmail.com>
2021-04-26 11:05:40 -0500

    added initial prompts

--- index.js
+++ index.js
@@ -2,3 +2,58 @@ const mysql = require('mysql');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 
+const connection = mysql.createConnection({
+	host: 'localhost',
+
+	port: 3306,
+
+	user: 'root',
+
+	password: 'sunflower2',
+	database: 'top_songsDB'
+});
+
+const initialPrompts = () => {
+	console.log('Welcome to the Employee Management System!\n');
+	inquirer
+		.prompt({
+			name: 'choiceList',
+			type: 'list',
+			message: 'Please choose an option.',
+			choices: [ 'Add data', 'View data', 'Update employees', 'Exit' ]
+		})
+		.then((choice) => {
+			switch (choice.choiceList) {
+				//Starts prompts for adding data
+				case 'Add data':
+					addDataPrompts();
+					break;
+
+				//Starts prompts for viewing data
+				case 'View data':
+					viewDataPrompts();
+					break;
+
+				//Starts prompts for updating data
+				case 'Update employees':
+					updateDataPrompts();
+					break;
+
+				//Exits prompts
+				case 'Exit':
+					connection.end();
+					process.exit(1);
+					break;
+
+				default:
+					console.log('Invalid choice.');
+					break;
+			}
+		});
+};
+
+const addDataPrompts = () => {};
+
+const viewDataPrompts = () => {};
+
+const updateDataPrompts = () => {};
